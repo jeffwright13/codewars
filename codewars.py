9762b86b7fd0468a25b68be1cd2943f37d9e3ab0
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-26 15:15:45 -0400

    some stuff

---
+++ codewars.py
@@ -0,0 +1,17 @@
+def main():
+    print function(None)
+
+def diamond(n):
+    """
+    print some diamonds in ASCII
+    """
+    return None
+
+def test_function():
+    assert diamond(0) == None
+    assert diamond(2) == None
+    assert diamond(1) == '*'
+
+if __name__ == "__main__":
+    main()
+    
