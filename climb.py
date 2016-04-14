5f19870c464cbd874e8de42e51c551a2fc9b1f13
Jeff Wright <jeff.washcloth@gmail.com>
2016-04-14 15:06:28 -0400

    Completed and working

--- climb.py
+++ climb.py
@@ -3,12 +3,17 @@ def main():
 
 def climb(n):
     """
-    For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1. For example, given N = 13, the sequence is [1, 3, 6, 13].
+    For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either double the preceeding number, or double plus 1. For example, given N = 13, the sequence is [1, 3, 6, 13].
 
     Write a function that returns this sequence given a possitive number N. Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prepending the elements to a list.
     """
     if n <= 0:
         return 'Error'
+
+    seq = [1]
+    for i in range(1, n):
+        seq.append(
+    
 
     return []
 
--- find_next_square.py
+++ find_next_square.py
@@ -3,18 +3,46 @@ def main():
 
 def find_next_square(sq):
     """
-    For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either double the preceeding number, or double plus 1. For example, given N = 13, the sequence is [1, 3, 6, 13].
+    You might know some pretty large perfect squares. But what about the NEXT one?
 
-    Write a function that returns this sequence given a possitive number N. Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prepending the elements to a list.
+    Complete the findNextSquare method that finds the next integer perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+    If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+    Examples:
+
+    findNextSquare(121) --> returns 144
+    findNextSquare(625) --> returns 676
+    findNextSquare(114) --> returns -1 since 114 is not a perfect
     """
-    if sq <= 0:
+    if sq < 0:
         return -1
+    if sq == 0:
+        return 1
+    if sq == 1:
+        return 4
+
+    def is_square(apositiveint):
+      x = apositiveint // 2
+      seen = set([x])
+      while x * x != apositiveint:
+        x = (x + (apositiveint // x)) // 2
+        if x in seen: return False
+        seen.add(x)
+      return True
+    
+    if not is_square(sq):
+        return -1
+
+    import math
+    return (math.sqrt(sq) + 1) ** 2
+    
 
 def test_find_next_square():
     assert find_next_square(-1) == -1        
-    assert find_next_square(0)  == -1    
+    assert find_next_square(0)  == 1    
     assert find_next_square(47) == -1
-    assert find_next_square(1)  == [1]
+    assert find_next_square(1)  == 4
     assert find_next_square(121) == 144
     assert find_next_square(625) == 676
     assert find_next_square(319225) == 320356
