0c245df68d06d656593118374e39e97cc6889985
Jeff Wright <jeff.washcloth@gmail.com>
2016-04-15 10:17:42 -0400

    Unit tests added (but failing, of course)

--- days_until_christmas.py
+++ days_until_christmas.py
@@ -10,6 +10,8 @@ def days_until_christmas(day):
     Your function should take 1 argument (a Date object) which will be the day of the year it is currently. The function should then work out how many days it is until Christmas.
 
     Watch out for leap years!
+
+    NOTE: date object doc at https://docs.python.org/2/library/datetime.html#date-objects.
     """
     import datetime
     print type(day)
@@ -17,7 +19,18 @@ def days_until_christmas(day):
     return 0
 
 def test_days_until_christmas():
-    assert days_until_christmas(None) == None
+    from datetime import date
+    assert days_until_christmas(None)             == None
+    assert days_until_christmas(date(2016,12,9))  == 16
+    assert days_until_christmas(date(2016,12,8))  == 17
+    assert days_until_christmas(date(1996,12,7))  == 18
+    assert days_until_christmas(date(2015,2,23))  == 305
+    assert days_until_christmas(date(2001,7,11))  == 167
+    assert days_until_christmas(date(2000,12,9))  == 16
+    assert days_until_christmas(date(1978,3,18))  == 282
+    assert days_until_christmas(date(2016,12,25)) == 0
+    assert days_until_christmas(date(2016,12,26)) == 364
+    assert days_until_christmas(date(2015,12,26)) == 365
 
 if __name__ == "__main__":
     main()
