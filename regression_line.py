990014f9e303d9ade9f3feba70481d30e9eac902
Jeff Wright <jeff.washcloth@gmail.com>
2016-04-18 08:05:23 -0400

    Stub code with failing unit tests

---
+++ regression_line.py
@@ -0,0 +1,16 @@
+def main():
+    print regression_line(None)
+
+def regression_line(x, y):
+    """ Return the a (intercept)
+        and b (slope) of Regression Line 
+        (Y on X).
+    """
+    pass
+
+def test_regression_line():
+    assert regression_line(None) == None
+
+if __name__ == "__main__":
+    main()
+    
