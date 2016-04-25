f0f3fbba0f63b870ddef2311cb00d95c80bf27fb
Jeff Wright <jeff.washcloth@gmail.com>
2016-04-24 21:20:54 -0400

    Tests to include classes. Needs debugging.

--- codewars.py
+++ codewars.py
@@ -5,12 +5,24 @@ def function(n):
     """
     """
     return n
+    
+def Class(object):
+	def __init__(self):
+		pass
+	def process(self, n):
+	    self.number = n
 
 def test_function():
     assert function(0) == 0
     assert function(1) == 1
     assert function(2) == 2
     assert function(3) == 2
+
+def test_class():
+	c = Class()
+	assert c.process(1)   == 1
+	assert c.process('k') == 'k'
+	assert c.process('2') == 3
 
 if __name__ == "__main__":
     main()
