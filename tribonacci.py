4e3363e5506b573dc4bf504b8b06210fba19e869
Jeff Wright <jeff.washcloth@gmail.com>
2016-05-05 09:43:26 -0400

    Fully working code with many unit tests (including non-ints)

--- tribonacci.py
+++ tribonacci.py
@@ -15,27 +15,33 @@ def tribonacci(signature, n):
 
     [0,0,1,1,2,4,7,13,24,...]
 
-    Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+    Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the seeded sequence.
 
-    Signature will always contain 3 numbers; n will always be a non-negative number; if n==0, then return an empty array and be ready for anything else which is not clearly specified ;)
+    Signature will always contain 3 numbers; n will always be a non-negative number; if n==0, return an empty array and be ready for anything else which is not clearly specified.
     """
     if n <=0:
         return []
     if 1 <= n <= 3:
         return signature[0:n]
     else:
-        pass 
-    
+        this = tribonacci(signature, n-1)
+        this.append(sum(this[n-4:n]))
+        return this    
 
 def test_tribonacci():
     assert tribonacci([0,1,2], 0) == []
     assert tribonacci([0,1,2], 1) == [0]
-    assert tribonacci([1,1,2], 2) == [1,1]
-    assert tribonacci([1,1,1], 3) == [1,1,1]
+    assert tribonacci([2,1,0], 1) == [2]
+    assert tribonacci([0,1,2], 2) == [0,1]
+    assert tribonacci([1,3,1], 3) == [1,3,1]
+    assert tribonacci([1,1,1], 4) == [1,1,1,3]
     assert tribonacci([1,1,3], 4) == [1,1,3,5]
+    assert tribonacci([1,1,3], 5) == [1,1,3,5,9]
     assert tribonacci([1,1,1], 8) == [1,1,1,3,5,9,17,31]
     assert tribonacci([0,0,1], 9) == [0,0,1,1,2,4,7,13,24]
-    assert tribonacci([1,1,2], 10) == [1,1,1,3,5,9,17,31,57,105]
+    assert tribonacci([1,1,1], 10) == [1,1,1,3,5,9,17,31,57,105]
+    assert tribonacci([3,2,1], 10) == [3,2,1,6,9,16,31,56,103,190]
+    assert tribonacci([0.5,0.5,0.5],30) == [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]
 
 if __name__ == "__main__":
     main()
