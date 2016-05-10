7c18f84e9df3fd7602e7ca7405713503b04dac59
Jeff Wright <jeff.washcloth@gmail.com>
2016-05-10 18:30:51 -0400

    Funtional and tested routines for calculating primality (is_prime), and for generating primes (gen_primes and gen_primes_range)

--- primes.py
+++ primes.py
@@ -7,29 +7,47 @@ def is_prime(n):
     if type(n) != int or n <= 1:
         return False
 
-    #print 'checking for n =', n
     for i in range(2, int(round(sqrt(n)))+1):
         if n % i == 0:
             return False
     return True
 
-def gen_primes(m, n):
+def gen_primes(m):
     """
-    Generate a list of primes on range [m, n] (inclusive)
-    Uses the sieve method
+    Generate a list of primes on range [2, n], inclusive,
+    by eliminating evens and then checking if prime
+    """
+    primes = [2]
+    for i in range(3, m+1, 2):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+def gen_primes_range(m, n):
+    """
+    Generate a list of primes on range [m, n], inclusive,
+    by eliminating evens and then checking if prime
     """
     if m < 0 or n < 0 or n < m:
         return []
 
-    if n % 2 == 0:
-        primes = range(n, m-1, -2)
+    if n == m:
+        if is_prime(n):
+            return [n]
+        else:
+            return []
+
+    if m % 2 == 0:
+        low = m + 1
     else:
-        primes = range(n-1, m-1, -2)
-    print "primes:", primes
-    for i in primes:
-        if not is_prime(i):
-            primes.remove(i)
-    primes.reverse()
+        low = m
+
+    primes = []
+    if m == 2:
+        primes.append(2)
+    for i in range(low, n+1, 2):
+        if is_prime(i):
+            primes.append(i)
     return primes
 
 def test_is_prime():
@@ -51,8 +69,17 @@ def test_is_prime():
     assert is_prime(15485863) == True
 
 def test_gen_primes():
-    assert gen_primes(2, 3) == [2, 3]
-    assert gen_primes(2, 4) == [2, 3]
-    assert gen_primes(3, 12) == [3, 5, 7, 11]
-    assert gen_primes(13, 56) == [13, 17, 19, 23, 29, 31, 27, 41, 43, 53]
+    assert gen_primes(2) == [2]
+    assert gen_primes(3) == [2, 3]
+    assert gen_primes(11) == [2, 3, 5, 7, 11]
+    assert gen_primes(56) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+
+def test_gen_primes_range():
+    assert gen_primes_range(2, 2) == [2]
+    assert gen_primes_range(3, 3) == [3]
+    assert gen_primes_range(2, 3) == [2, 3]
+    assert gen_primes_range(2, 4) == [2, 3]
+    assert gen_primes_range(2, 11) == [2, 3, 5, 7, 11]
+    assert gen_primes_range(3, 12) == [3, 5, 7, 11]
+    assert gen_primes_range(13, 56) == [13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 
