d37d435fc4c4bb6406d44e3618e91c37e7821515
Jeff Wright <jeff.washcloth@gmail.com>
2016-05-18 08:51:17 -0400

    Refactored function is_valid_HK_phone_number() to use regular expressions

--- valid_HK_ph_num.py
+++ valid_HK_ph_num.py
@@ -1,13 +1,10 @@
 def is_valid_HK_phone_number(number):
-    import string
-    parts = number.split(' ')
-    if len(parts) != 2:
+    import re
+    p = re.compile('^\d{4}[ ]\d{4}$')
+    if p.search(number):
+        return True
+    else:
         return False
-    for part in parts:
-        for char in part:
-            if char not in string.digits:
-                return False
-    return True
 
 def has_valid_HK_phone_number(number):
     import re
