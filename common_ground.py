33cceeb73a680d50c2bbea5337e4b1250cb4d249
Jeff Wright <jeff.washcloth@gmail.com>
2016-05-19 11:20:14 -0400

    Completed kata with passing tests

--- common_ground.py
+++ common_ground.py
@@ -13,12 +13,24 @@ def common_ground(s1, s2):
 
     If they are saying nothing in common, kill both samurai and blame a ninja. (output "death")
     """
-    return None
+    if not set(s1.split()).intersection(s2.split()):
+        return 'death'
+
+    common = []
+    for word in s2.split():
+        if word in s1.split():
+            if word not in common:
+                common.append(word)
+            continue
+
+    print "s1, s2, common:", s1, s2, common
+    return ' '.join(common)
 
 def test_common_ground():
+    assert common_ground("i like turtles", "what are you talking about") == 'death'
+    assert common_ground("", "") == 'death'
     assert common_ground("eat chicken", "eat chicken and rice") == 'eat chicken'
     assert common_ground("eat a burger and drink a coke", "drink a coke") == 'drink a coke'
-    assert common_ground("i like turtles", "what are you talking about") == 'death'
     assert common_ground("aa bb cc", "bb cc bb aa") == 'bb cc aa'
 
 if __name__ == "__main__":
