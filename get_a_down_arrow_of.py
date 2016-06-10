d9290d629e22fa6d7802f2375ac798e411c41a36
Jeff Wright <jeff.washcloth@gmail.com>
2016-06-10 09:13:22 -0400

    String manipulation for ascending/descnding sequence is in place

--- get_a_down_arrow_of.py
+++ get_a_down_arrow_of.py
@@ -3,6 +3,11 @@ def main():
 
 def get_a_down_arrow_of(n):
     """
+    Down Arrow With Numbers
+    =======================
+    https://www.codewars.com/kata/down-arrow-with-numbers
+    =====================================================
+
     Given a number n, make a down arrow shaped pattern.
 
     For example, when n = 5, the output would be:
@@ -37,15 +42,19 @@ def get_a_down_arrow_of(n):
     if n == 1:
         return '1'
 
-    return get_a_down_arrow_of(n-1) + '\n'
+    s = ''
+    s += ''.join([str(i) for i in range(1, n+1)])
+    s += ''.join([str(i) for i in range(n-1, 0, -1)])
+    
+ 
 
 def test_get_a_down_arrow_of():
-    assert get_a_down_arrow_of(0) == ''
+    assert get_a_down_arrow_of(0)  == ''
     assert get_a_down_arrow_of(-1) == ''
-    assert get_a_down_arrow_of(1) == '1'
-    assert get_a_down_arrow_of(2) == '121\n 1'
-    assert get_a_down_arrow_of(3) == '112321\n 121\n  1'
-    assert get_a_down_arrow_of(5) == '123454321\n 1234321\n  12321\n   121\n    1'
+    assert get_a_down_arrow_of(1)  == '1'
+    assert get_a_down_arrow_of(2)  == '121\n 1'
+    assert get_a_down_arrow_of(3)  == '112321\n 121\n  1'
+    assert get_a_down_arrow_of(5)  == '123454321\n 1234321\n  12321\n   121\n    1'
     assert get_a_down_arrow_of(11) == '123456789010987654321\n 1234567890987654321\n  12345678987654321123454321\n   123456787654321\n    1234567654321\n     12345654321\n      123454321\n       1234321\n        12321\n         121\n          1'
 
 if __name__ == "__main__":
