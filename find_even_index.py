2648198bb28981a55837e0dade9c2d54ffcd756e
Jeff Wright <jeff.washcloth@gmail.com>
2016-07-14 12:33:54 -0400

    Initial stub code with failing unit tests

---
+++ find_even_index.py
@@ -0,0 +1,36 @@
+def main():
+    print find_even_index.__doc__
+
+def find_even_index(arr):
+    """
+    Equal Sides Of An Array
+    (https://www.codewars.com/kata/equal-sides-of-an-array)
+    
+    You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+    For example:
+
+    Let's say you are given the array {1,2,3,4,3,2,1}:
+    Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+    Let's look at another one.
+    You are given the array {1,100,50,-51,1,1}:
+    Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+    Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+    Input:
+
+    An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+    Output:
+
+    The index N such as the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+    """
+    return None
+
+def test_find_even_index():
+    assert find_even_index([1,2,3,4,3,2,1]) == 3
+    assert find_even_index([1,100,50,-51,1,1]) == 1
+    assert find_even_index(1,2,3,4,5,6) == -1
+    assert find_even_index([0,0,0,0,0]) == 0
+
+if __name__ == "__main__":
+    main()
