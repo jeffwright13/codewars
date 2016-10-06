ed4413f9a161e6b9768c4a3fd5ce254e8ce97181
Jeff Wright <jeff.washcloth@gmail.com>
2016-10-06 11:28:09 -0400

    Initial stub code with failing unit tests

---
+++ perimeter.py
@@ -0,0 +1,41 @@
+def main():
+    print perimeter.__doc__
+
+def perimeter(n):
+    """
+https://www.codewars.com/kata/perimeter-of-squares-in-a-rectangle
+
+The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
+
+Say that S(n) is the nth term of the above sum. So
+
+S(0) = 1, S(1) = 1, S(2) = 2, ... , S(5) = 8
+
+Could you give the sum S of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+S = S(0) + S(1) + ... + S(n) ?
+
+alternative text
+Hint:
+
+See Fibonacci sequence and beware of rather big n:-)
+Ref:
+
+http://oeis.org/A000045
+
+The perimeter perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+perimeter(5)  should return 80
+perimeter(7)  should return 216
+    """
+    return None
+
+def test_perimeter():
+    assert perimeter(5) == 80
+    assert perimeter(7) == 216
+    assert perimeter(20) == 114624
+    assert perimeter(30) == 14098308
+    assert perimeter(100) == 6002082144827584333104
+
+if __name__ == "__main__":
+    main()
