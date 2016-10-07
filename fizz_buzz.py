1c69d69359cd71ab4dd0bbc9e25b478a0d45e97a
Jeff Wright <jeff.washcloth@gmail.com>
2016-10-07 13:52:33 -0400

    Working code - submitted

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -10,9 +10,19 @@ Write a function that takes an integer and returns an Array [A, B, C] where A is
 For example, solution(20) should return [5, 2, 1]
     """
     A = B = C = 0
-    for in in range(1,number+1):
-        pass
 
+    for i in range(1, number):
+        three = (i % 3 == 0)
+        five  = (i % 5 == 0)
+
+        print i, three, five
+
+        if three and not five: A += 1
+        if five and not three: B += 1
+        if three and five:     C += 1
+
+    return [A,B,C]
+        
 def test_solution():
     assert solution(20) == [5,2,1]
     assert solution(2) == [0,0,0]
