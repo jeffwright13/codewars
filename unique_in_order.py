7a30db6e82718d46ed896cfe4fb0d7b76a5a838e
Jeff Wright <jeff.washcloth@gmail.com>
2016-10-27 10:02:31 -0400

    Working code, submitted.

--- unique_in_order.py
+++ unique_in_order.py
@@ -13,7 +13,7 @@ unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 unique_in_order([1,2,2,3,3])       == [1,2,3]
     """
-    return None
+    return [x for i,x in enumerate(iterable) if i is 0 or x is not iterable[i-1]]
 
 def test_unique_in_order():
     assert unique_in_order('AAAABBBCCDAABBB') == ['A','B','C','D','A','B']
