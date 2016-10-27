a9cd23b9e50f10098f25b515b2722d315382fac0
Jeff Wright <jeff.washcloth@gmail.com>
2016-10-27 07:43:52 -0400

    Initial stub code with failing unit tests

---
+++ revrot.py
@@ -0,0 +1,43 @@
+def main():
+    print revrot.__doc__
+
+def revrot(strng, sz):
+    """
+    https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/
+    
+    The input is a string str of digits. Cut the string into chunks of size sz (ignore the last chunk if its size is less than sz).
+
+    If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse it; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+    If:
+        sz is <= 0 or if str is empty, return ""
+        sz is greater (>) than the length of str it is impossible to take a chunk of size sz, hence return ""
+
+    Examples:
+    revrot("123456987654", 6) --> "234561876549"
+    revrot("123456987653", 6) --> "234561356789"
+    revrot("66443875", 4) --> "44668753"
+    revrot("66443875", 8) --> "64438756"
+    revrot("664438769", 8) --> "67834466"
+    revrot("123456779", 8) --> "23456771"
+    revrot("", 8) --> ""
+    revrot("123456779", 0) --> ""
+    """
+    if len(strng) == 0 or sz == 0 or sz > len(strng): return ''
+    
+    chunk = []
+    print range(0, len(strng)/sz)
+    for i in range(0, len(strng)/sz):
+        chunk[i] = strng[i:i+sz]
+    print chunk
+
+def test_revrot():
+    assert revrot('1234', 0) == ''
+    assert revrot('', 1) == ''
+    assert revrot('', 0) == ''
+    assert revrot('1234', 5) == ''
+    assert revrot('733049910872815764', 5) == ''
+    assert revrot('123456987653', 6) == '234561356789'
+
+if __name__ == "__main__":
+    main()
